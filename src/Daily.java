4bdbd68642ee46a8dfd40739c89eaac09519bebe
Saveliev Mikhail <Savelievmv@gmail.com>
2016-12-13 19:43:12 +0100

    Superclass Appointment with methods and subClass Onetime, Daily

--- src/Daily.java
+++ src/Daily.java
@@ -1,9 +1,13 @@
 /**
  * Created by Михаил on 13.12.2016.
  */
-public class Daily {
+public class Daily extends Appointment {
 
-    public Daily() {
+    public Daily(String dscr) {
+        super(dscr, 0, 0, 0);
+    }
 
+    public boolean occursOn(int y, int m, int d) {
+        return true;
     }
 }
