48fb9ce1c4bb0639fd0762c77f7d9ec778d0ca2b
Saveliev Mikhail <Savelievmv@gmail.com>
2016-12-13 19:50:52 +0100

    subclass Monthly

--- src/Monthly.java
+++ src/Monthly.java
@@ -1,10 +1,14 @@
 /**
  * Created by Михаил on 13.12.2016.
  */
-public class Monthly {
+public class Monthly extends Appointment{
 
-    public Monthly() {
+    public Monthly(String dscr, int d) {
+        super(dscr, 0, 0, d);
+    }
 
+    public boolean occursOn(int y, int m, int d) {
+        return super.getDay() == d;
     }
 
 
